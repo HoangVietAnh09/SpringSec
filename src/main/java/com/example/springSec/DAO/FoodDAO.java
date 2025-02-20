1751517cece296727710136e2f079dc5d7ecb176
Hoang Viet Anh <vietanh092004@gmail.com>
2025-02-20 15:23:56 +0700

    Add execute SQL query via EntiryManager

---
+++ src/main/java/com/example/springSec/DAO/FoodDAO.java
@@ -0,0 +1,26 @@
+package com.example.springSec.DAO;
+
+
+import com.example.springSec.Entity.Food;
+import jakarta.persistence.EntityManager;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+@Component
+public class FoodDAO {
+
+    EntityManager entityManager;
+
+
+    public List<Food> getFoodByName(String name) {
+        var query = "FROM Food WHERE name = '" + name + "'";
+        var exe = entityManager.createQuery(query, Food.class);
+        return exe.getResultList();
+    }
+}
