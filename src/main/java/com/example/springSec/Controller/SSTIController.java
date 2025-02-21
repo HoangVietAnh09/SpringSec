5460a2f6061cf0c048bff72647b8b435bad760aa
Hoang Viet Anh <vietanh092004@gmail.com>
2025-02-21 10:55:06 +0700

    Fix SSTI controller

--- src/main/java/com/example/springSec/Controller/SSTIController.java
+++ src/main/java/com/example/springSec/Controller/SSTIController.java
@@ -6,11 +6,18 @@ import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.apache.velocity.VelocityContext;
 import org.apache.velocity.app.Velocity;
+import org.apache.velocity.app.VelocityEngine;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.io.StringWriter;
+import java.lang.module.Configuration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 @RestController
 @RequestMapping("/ssti")
@@ -19,16 +26,21 @@ import java.io.StringWriter;
 public class SSTIController {
 
     @GetMapping("/velocity")
-    public void velocity(String template) {
-        Velocity.init();
+    public String velocity(@RequestParam String input) {
+        Properties props = new Properties();
+//        props.setProperty("runtime.log.logsystem.class", "org.apache.velocity.runtime.log.NullLogChute"); // Tắt logging
+        VelocityEngine velocityEngine = new VelocityEngine();
+        velocityEngine.init(props);
 
         VelocityContext context = new VelocityContext();
 
-        context.put("author", "Elliot A.");
-        context.put("address", "217 E Broadway");
-        context.put("phone", "555-1337");
+        StringWriter writer = new StringWriter();
+        Velocity.evaluate(context, writer, "test", input);
 
-        StringWriter swOut = new StringWriter();
-        Velocity.evaluate(context, swOut, "test", template);
+        //payload clear text: #set($e="e");$e.getClass().forName("java.lang.Runtime").getMethod("getRuntime",null).invoke(null,null).exec("cmd.exe /c calc")
+        System.out.println(writer);
+        return "Xin chao " + writer;
     }
+
+
 }
